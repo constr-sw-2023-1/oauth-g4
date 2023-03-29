cf262a237a370b1048d3b08b0f9bbedaa2a9d518
Henrique Oliveira <h.oliveira@edu.pucrs.br>
2023-03-29 20:03:22 -0300

    Initialize Kotlin Project

---
+++ src/main/kotlin/br/edu/pucrs/group4/oauthg4/OauthG4Application.kt
@@ -0,0 +1,11 @@
+package br.edu.pucrs.group4.oauthg4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OauthG4Application
+
+fun main(args: Array<String>) {
+	runApplication<OauthG4Application>(*args)
+}
---
+++ src/test/kotlin/br/edu/pucrs/group4/oauthg4/OauthG4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.edu.pucrs.group4.oauthg4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OauthG4ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
