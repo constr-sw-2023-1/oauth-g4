257130ff804bd2bb7028f725dfb7f8ce16ebaabc
LeonardoBerlatto <leoberlatto@gmail.com>
2023-04-11 12:29:08 -0300

    Adding spring security initial config

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	// security
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
