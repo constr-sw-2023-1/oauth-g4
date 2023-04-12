e41185db9626750d5c08a887a7dcaa87b567f6b7
LeonardoBerlatto <leoberlatto@gmail.com>
2023-04-11 21:30:22 -0300

    Fixing

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	// security
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
