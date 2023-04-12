1ece86cfe746a5d95e1c1c0453e6eeb886a18af3
LeonardoBerlatto <leoberlatto@gmail.com>
2023-04-11 21:30:22 -0300

    Fixing

--- src/main/kotlin/br/edu/pucrs/group4/oauthg4/adapter/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/br/edu/pucrs/group4/oauthg4/adapter/repository/UserRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package br.edu.pucrs.group4.oauthg4.adapter.repository
 
+import br.edu.pucrs.group4.oauthg4.adapter.keycloak.KeycloakUserRepository
 import br.edu.pucrs.group4.oauthg4.adapter.representation.keycloak.UserRepresentation
 import br.edu.pucrs.group4.oauthg4.domain.dto.UserDTO
 import br.edu.pucrs.group4.oauthg4.domain.entity.User
@@ -8,33 +9,35 @@ import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-class UserRepositoryImpl() : UserRepository {
-
+class UserRepositoryImpl(
+    private val keycloakUserRepository: KeycloakUserRepository
+) : UserRepository {
     override fun findAll(): List<User> {
-        TODO("Not yet implemented")
+        return keycloakUserRepository.findAll()
     }
 
     override fun findById(id: UUID): Optional<User> {
-        TODO("Not yet implemented")
+        return keycloakUserRepository.findById(id)
     }
 
     override fun findByEmail(email: String): Optional<User> {
-        TODO("Not yet implemented")
+        return Optional.empty()
     }
 
     override fun save(user: UserRepresentation, token: String): UserDTO {
-        TODO("Not yet implemented")
+        val saveResponse = keycloakUserRepository.save(user, token)
+
+        return UserDTO(user.id.toString(),user.username,user.firstName,user.lastName,true)
     }
 
     override fun update(user: User): User {
-        TODO("Not yet implemented")
+        return null!!
     }
 
     override fun updatePassword(id: Long, password: String) {
-        TODO("Not yet implemented")
     }
 
     override fun delete(id: Long) {
-        TODO("Not yet implemented")
+        keycloakUserRepository.delete(id)
     }
 }
