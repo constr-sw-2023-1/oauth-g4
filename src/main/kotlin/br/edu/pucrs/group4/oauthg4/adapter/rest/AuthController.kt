d4220da9dc552c0932ab853d0ab20d1d4dd52630
LeonardoBerlatto <leoberlatto@gmail.com>
2023-04-12 19:31:03 -0300

    Adding auth controller swagger config

--- src/main/kotlin/br/edu/pucrs/group4/oauthg4/adapter/rest/AuthController.kt
+++ src/main/kotlin/br/edu/pucrs/group4/oauthg4/adapter/rest/AuthController.kt
@@ -1,6 +1,7 @@
 package br.edu.pucrs.group4.oauthg4.adapter.rest
 
 import br.edu.pucrs.group4.oauthg4.adapter.representation.request.LoginRequestDTO
+import br.edu.pucrs.group4.oauthg4.adapter.rest.api.AuthApi
 import br.edu.pucrs.group4.oauthg4.domain.dto.JwtTokenDTO
 import br.edu.pucrs.group4.oauthg4.domain.service.AuthService
 import org.springframework.http.ResponseEntity
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class AuthController(
     private val authService: AuthService
-) {
+): AuthApi {
     @PostMapping("/login")
-    fun login(
+    override fun login(
         @RequestParam("client_id") clientId: String,
         @RequestParam("client_secret") clientSecret: String,
         @RequestParam("username") username: String,
