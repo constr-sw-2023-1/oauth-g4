d7b001b9b70ab9ab7a476d608070e1e5f6b33f2b
LeonardoBerlatto <leoberlatto@gmail.com>
2023-04-16 20:17:09 -0300

    Changing message for auth exception

--- src/main/kotlin/br/edu/pucrs/group4/oauthg4/domain/exception/AuthenticationException.kt
+++ src/main/kotlin/br/edu/pucrs/group4/oauthg4/domain/exception/AuthenticationException.kt
@@ -3,7 +3,7 @@ package br.edu.pucrs.group4.oauthg4.domain.exception
 import org.springframework.http.HttpStatus
 
 class AuthenticationException(
-    override val message: String = "Invalid Credentials: wrong username or password",
+    override val message: String = "Invalid or missing credentials",
     override val code: String = "G4-001",
     override val status: HttpStatus = HttpStatus.UNAUTHORIZED)
     : RuntimeException(message), RestException
