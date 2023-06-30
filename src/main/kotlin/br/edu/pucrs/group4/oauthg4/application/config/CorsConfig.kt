afc3f703a5221b50e6fb5a6e04f19b98e54ac5c2
Leonardo Berlatto <leoberlatto@gmail.com>
2023-06-30 09:19:49 -0300

    Adding cors initial config

---
+++ src/main/kotlin/br/edu/pucrs/group4/oauthg4/application/config/CorsConfig.kt
@@ -0,0 +1,15 @@
+package br.edu.pucrs.group4.oauthg4.application.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+    }
+}
